580fa06524ae555bd130b048297c8440248dbeb2
JungHyunjun <103713434+ark1210@users.noreply.github.com>
2023-04-28 13:59:53 +0900

    final_read_exactly

--- read_file.c
+++ read_file.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+
+           #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_LENGTH 100
 
@@ -16,9 +18,11 @@ struct instruction {
 };
 
 union value {
-    signed char num;
+    unsigned char num;
     struct instruction inst;
 };
+
+int flag[256] = { 0 };
 
 char * read_a_line (FILE *code_txt){
     static char buf[256];
@@ -57,73 +61,80 @@ const char *enumToString(enum operator op) {
 enum operator toEnum(char *token) {
     if (strcmp(token, "READ") == 0) {
         return READ;
-    } else if (strcmp(token, "WRITE") == 0) {
-        return WRITE;
-    } else if (strcmp(token, "ASSIGN") == 0) {
-        return ASSIGN;
+    } else if (strcmp(token, "EQ") == 0) {
+        return EQ;
+    } else if (strcmp(token, "JUMPIF") == 0) {
+        return JUMPIF;
     } else if (strcmp(token, "MOVE") == 0) {
         return MOVE;
-    } else if (strcmp(token, "LOAD") == 0) {
-        return LOAD;
-    } else if (strcmp(token, "STORE") == 0) {
-        return STORE;
-    } else if (strcmp(token, "ADD") == 0) {
-        return ADD;
-    }else if (strcmp(token, "MINUS") == 0) {
+    } else if (strcmp(token, "MINUS") == 0) {
         return MINUS;
-    }else if (strcmp(token, "MULT") == 0) {
-        return MULT;
-    }else if (strcmp(token, "MOD") == 0) {
-        return MOD;
-    }else if (strcmp(token, "EQ") == 0) {
-        return EQ;
-    }else if (strcmp(token, "LESS") == 0) {
-        return LESS;
-    }else if (strcmp(token, "JUMP") == 0) {
+    } else if (strcmp(token, "JUMP") == 0) {
         return JUMP;
+    } else if (strcmp(token, "WRITE") == 0) {
+        return WRITE;
+    } else
+        return TERM;
+
+}int atoi_v2(char * token){
+    char * temp;
+    int result;
+
+    // token의 길이에 맞춰 동적 메모리 할당
+    temp = (char *)malloc(strlen(token) + 1);
+    if(temp == NULL){
+        printf("Memory allocation failed.\n");
+        return -1;
     }
-    else if (strcmp(token, "JUMPIF") == 0) {
-        return JUMPIF;
-    }else 
-            return TERM;
+
+    for(int i = 0; i < strlen(token); i++){
+        if(isdigit(token[i])){
+            //strcat을 하기 위해 tot 문자열 배열을 선언하고 맨 끝에 null을 넣어줌
+            char tot[2] = { token[i], '\0' };
+            strcat(temp, tot);
+        }
+    }
+
+    result = atoi(temp);
+    free(temp);
+    return result;
 }
 
-void load_on_union(char * s, union value *memArray) {
-    if (s == NULL) {
-        return;
-    }
-
-    char input_line[256];
-    strcpy(input_line, s);
-
+void load_on_union(char * s, union value *memArray){
+    // ':'을 기준으로 union의 인덱스와 명령어를 구분
     char * token;
     char * remain_sentence;
-    token = strtok_r(input_line, ":", &remain_sentence);
+    token = strtok_r(s, ":", &remain_sentence); // '\n'을 추가한 구분자
     int idx = atoi(token);
     token = strtok_r(NULL, " ", &remain_sentence);
+    // 명령어가 value인가 instruction인가?
     if (token != NULL && strstr(token, "\"") != NULL) {
-        char temp;
-        sscanf(token, "\"%c\"", &temp);
-        memArray[idx].num = temp;
+        // value
+        char * temp;
+        sscanf(token, "\"%s\"", temp);
+        printf("here : %d\n", atoi_v2(temp));
+        memArray[idx].num = atoi_v2(temp);
+        flag[idx] = 1;
     }
     else {
+        // instruction
         int i = 0;
         while(token){
             if(strcmp(token, "9") > 0){
+                // operator
                 memArray[idx].inst.op = toEnum(token);
             } else {
+                // operand
                 int operand = atoi(token);
                 if (i == 0) memArray[idx].inst.r1 = operand;
                 else if (i == 1) memArray[idx].inst.r2 = operand;
                 else if (i == 2) memArray[idx].inst.r3 = operand;
                 i++;
             }
-            token = strtok_r(NULL, " ", &remain_sentence);
+            token = strtok_r(NULL, " ", &remain_sentence); // '\n'을 추가한 구분자
         }
     }
 }
-
-
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -133,6 +144,7 @@ int main(int argc, char *argv[]) {
 
     FILE *assemblyFile = fopen(argv[1], "r");
 
+    printf("%s\n",argv[1]);
     if (assemblyFile == NULL) {
         printf("Error: 파일을 열 수 없습니다.\n");
         return 1;
@@ -150,14 +162,12 @@ int main(int argc, char *argv[]) {
     fclose(assemblyFile);
 
       // memArray 배열의 흐름을 출력
-    for (int i = 0; i < 25; i++) {
-        if (memArray[i].inst.op >= 0 && memArray[i].inst.op <= 14) {
-            printf("memArray[%d].inst.op = %s;\n", i, enumToString(memArray[i].inst.op));
-            printf("memArray[%d].inst.r1 = %d;\n", i, memArray[i].inst.r1);
-            printf("memArray[%d].inst.r2 = %d;\n", i, memArray[i].inst.r2);
-            printf("memArray[%d].inst.r3 = %d;\n", i, memArray[i].inst.r3);
-        } else if (memArray[i].num != 0) {
-            printf("memArray[%d].num = '%c';\n", i, memArray[i].num);
+    for (int i = 0; i < 256; i++) {
+        if(flag[i] == 1){
+            printf("memArray[%d].num = %d\n", i, memArray[i].num);
+        }else{
+            printf("memArray[%d].inst.operator = %u\n", i, memArray[i].inst.op);
+            printf("memArray[%d].inst.operand = %d, %d, %d\n", i, memArray[i].inst.r1, memArray[i].inst.r2, memArray[i].inst.r3);
         }
     }
 
